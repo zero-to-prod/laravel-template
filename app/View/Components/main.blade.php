6701dd80997c6b1e29d72b05d5237cba1c82b23e
david_smith <david_smith@sweetwater.com>
2026-08-13 19:17:01 -0400

    Comment out left navigation and move footer outside main content container
    - Comment out `<x-left-nav/>` component rendering
    - Move `<x-footer/>` outside the main content div to full width
    - Add `relative z-20` positioning to footer for proper stacking

--- app/View/Components/main.blade.php
+++ app/View/Components/main.blade.php
@@ -13,7 +13,7 @@
 <body class="h-screen overflow-y-scroll scrollbar-visible {{$Main->classnames}}">
 <x-topnav :topnav="$Main->topnav()"/>
 @if($Main->leftNav)
-  <x-left-nav/>
+{{--  <x-left-nav/>--}}
 @elseif($Main->adminNav)
   <x-admin-nav/>
 @elseif($Main->settingsNav)
@@ -21,8 +21,8 @@
 @endif
 <div @class(['mt-16', 'lg:pl-56' => $Main->nav()])>
   <div class="min-h-[calc(100vh-4rem)]">{{$slot}}</div>
-  <x-footer/>
 </div>
+<x-footer/>
 @vite('resources/js/app.js')
 </body>
 </html>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     use App\Routes\Web;
     use App\View\DataModels\Svg;
 @endphp
-<footer class="footer sm:footer-horizontal border-t border-base-300 bg-base-200 p-10 text-base-content">
+<footer class="footer sm:footer-horizontal relative z-20 border-t border-base-300 bg-base-200 p-10 text-base-content">
     <aside>
         <a href="{{Web::home->value}}" class="flex items-center gap-2" title="Go Home">
             <x-svg :svg="[Svg::name => 'logo', Svg::classname => 'h-8 w-8']"/>
